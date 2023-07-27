e93bb1fdbee32d538f0a918630b4b8f8299d3f70
PRIYOBROTO KARMAKAR <126158991+PRIYOBROTOKARMAKAR@users.noreply.github.com>
2023-07-27 22:25:31 +0530

    Hangman Word Game (#1)
    This repository contains a Python implementation of the classic word game Hangman. The game is played by guessing the letters of a secret word, one at a time. If the guessed letter is in the secret word, it is revealed. If the guessed letter is not in the secret word, the player loses a life. The player loses the game if they run out of lives.
    The game is implemented in Python and uses the random module to randomly select a secret word from a list of words. The game also uses the print() function to display the game state to the player.
    This repository is a good starting point for learning how to implement a word game in Python. It also provides a good example of how to use the random and print() functions in Python.

---
+++ Hangman/hangman.py
@@ -0,0 +1,54 @@
+import random
+
+
+
+from hangman_words import word_list
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+
+from hangman_art import logo
+print(logo)
+
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    #If the user has entered a letter they've already guessed, print the letter and let them know.
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+  
+    from hangman_art import stages
+    print(stages[lives])
